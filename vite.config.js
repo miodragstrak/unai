324f8a2dd5dd1aabcab67ff50043df5552fc794e
miodragstrak <miodrag.strak@gmailcom>
2025-12-21 20:33:59 +0100

    Deploy changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/unai/", // MUST match your repo name
+  base: "/", // MUST match your repo name
 });
